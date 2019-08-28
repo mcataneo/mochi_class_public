7ce1301fa389f819d22469b47a9f8d9eab5ecb30
Dvořáček Josef <jose@fzu.cz>
2019-08-28 15:57:01 +0200

    change jd

--- source/perturbations.c
+++ source/perturbations.c
@@ -2,6 +2,8 @@
  *
  * Julien Lesgourgues, 23.09.2010
  *
+ * asdasd You will experience a strong urge to do good; but it will pass. fdsadsf
+ *
  * Deals with the perturbation evolution.
  * This module has two purposes:
  *
