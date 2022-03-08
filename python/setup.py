f22b3868d2e14ab047beccdfc024e53feeaf2d04
Emilio Bellini <emilio.bellini@protonmail.com>
2022-03-08 17:05:23 +0100

    moved hiclass headers to gravity_Smg

--- python/setup.py
+++ python/setup.py
@@ -27,6 +27,7 @@ classy_folder = os.path.join(root_folder, "python")
 heat_folder = os.path.join(os.path.join(root_folder, "external"),"heating")
 recfast_folder = os.path.join(os.path.join(root_folder, "external"),"RecfastCLASS")
 hyrec_folder = os.path.join(os.path.join(root_folder, "external"),"HyRec2020")
+hiclass_folder = os.path.join(root_folder, "gravity_smg")
 
 # Recover the CLASS version
 with open(os.path.join(include_folder, 'common.h'), 'r') as v_file:
@@ -38,7 +39,7 @@ with open(os.path.join(include_folder, 'common.h'), 'r') as v_file:
 
 # Define cython extension and fix Python version
 classy_ext = Extension("classy", [os.path.join(classy_folder, "classy.pyx")],
-                           include_dirs=[nm.get_include(), include_folder, heat_folder, recfast_folder, hyrec_folder],
+                           include_dirs=[nm.get_include(), include_folder, heat_folder, recfast_folder, hyrec_folder, hiclass_folder],
                            libraries=liblist,
                            library_dirs=[root_folder, GCCPATH],
                            extra_link_args=['-lgomp']
